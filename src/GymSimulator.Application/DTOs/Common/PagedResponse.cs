e31d78d0a9758afe1b102e4611fc197f4a9483b9
YuriBoppre <yuriboppre@gmail.com>
2025-10-30 11:58:06 -0300

    Feat: Creating pagination request and response

---
+++ src/GymSimulator.Application/DTOs/Common/PagedResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSimulator.Application.DTOs.Common
+{
+    public class PagedResponse<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public bool HasPrevious => PageNumber > 1;
+        public bool HasNext => PageNumber < TotalPages;
+
+        public PagedResponse(List<T> items, int totalCount, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
